6849b8452d0bc4951eea7f9e4420e4948f92b166
Ibad18 <ibad1818@gmail.com>
2024-10-18 03:36:45 +0500

    App.mjs modified

--- App.mjs
+++ App.mjs
@@ -2,30 +2,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // This is important for JSON request body parsing
+
 // Middleware
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // Routes
 app.use('/api', userRoutes);
 
 // MongoDB connection
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((error) => console.error('MongoDB connection error:', error));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch((error) => console.log('Error connecting to MongoDB:', error));
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
